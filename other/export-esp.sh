0e75395403f0749950e1e35133879678303588b3
hiltar <43303122+hiltar@users.noreply.github.com>
2022-10-09 13:16:58 +0300

    Initial esp32 program

---
+++ other/export-esp.sh
@@ -0,0 +1,2 @@
+export LIBCLANG_PATH="/home/tarmo/.espressif/tools/xtensa-esp32-elf-clang/esp-14.0.0-20220415-x86_64-unknown-linux-gnu/lib/"
+export PATH="/home/tarmo/.espressif/tools/xtensa-esp32-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:/home/tarmo/.espressif/tools/xtensa-esp32s2-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:/home/tarmo/.espressif/tools/xtensa-esp32s3-elf-gcc/8_4_0-esp-2021r2-patch3-x86_64-unknown-linux-gnu/bin/:$PATH"
---
+++ other/install.sh
@@ -0,0 +1,3 @@
+curl -LO https://raw.githubusercontent.com/esp-rs/rust-build/main/install-rust-toolchain.sh
+chmod +x install-rust-toolchain.sh
+./install-rust-toolchain.sh
